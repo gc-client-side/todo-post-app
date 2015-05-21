7221eb62d6dec08d09cf0c5677ddd1db77e08a39
joehandlebar <schang09@gmail.com>
2015-05-20 20:42:00 -0400

    basic post templating

--- src/app/components/canvas/canvas.controller.js
+++ src/app/components/canvas/canvas.controller.js
@@ -3,7 +3,25 @@
 angular.module('todoPostApp')
   .controller('CanvasCtrl', function ($scope) {
     $scope.posts = [
-    	{ id: 1, title: 'Hello post' },
-    	{ id: 2, title: 'Hello post 2' }
+    	{ id: 1, 
+    	  title: 'Hello post',
+    	  description: 'That is very hello',
+    	  subtasks: ['be a man', 'be a girl', 'be a dog']
+    	},
+    	{ id: 2, 
+    	  title: 'Hello post 2',
+    	  description: 'It is over',
+    	  subtasks: ['go home', 'go school', 'be a dog']
+    	},
+    	{ id: 3, 
+    	  title: 'Hello post 2',
+    	  description: 'It is over',
+    	  subtasks: ['go home', 'go school', 'be a dog']
+    	},
+    	{ id: 4, 
+    	  title: 'Hello post 2',
+    	  description: 'It is over',
+    	  subtasks: ['go home', 'go school', 'be a dog']
+    	}
     ];
   });
